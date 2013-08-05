f3712b3b49486d84e9a1947714e2fdfd3ecdb8a7
nelson12# <aw.nelson@hotmail.com>
2013-08-04 22:34:31 -0400

    Function

---
+++ Wacky copy 3.js
@@ -0,0 +1,12 @@
+//adam nelson 08032013 Wacky Functions
+//How many smurfs will fit in a box?
+
+function smurfBox(){
+	var width =2;
+	var height = 3;
+	var length = 3;
+	var area = width*height*length;
+	console.log(area);
+}
+smurfBox();
+
