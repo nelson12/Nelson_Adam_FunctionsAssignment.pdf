7e6f66e48ee941d3533615b3b394c606ae65b79d
nelson12# <aw.nelson@hotmail.com>
2013-08-04 21:15:32 -0400

    Declaring a variable and starting function code

---
+++ Personal Functions copy.js
@@ -0,0 +1,5 @@
+//adam nelson 08032013 Personal Functions
+//What will happen if my daughter wakes up too many times in one night?
+
+function timesAwake(){
+	var a = prompt("I am figuring out what happens when a child wakes up too mnay times a night! How many times does Izzy wake up between 8 and midnight?");//declaring variable
