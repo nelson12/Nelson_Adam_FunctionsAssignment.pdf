a3e32a6f04222267a856f26bab7322686a3bfd20
nelson12# <aw.nelson@hotmail.com>
2013-08-04 22:13:45 -0400

    Prompt

---
+++ Industry Function copy 2.js
@@ -0,0 +1,7 @@
+//adam nelson 08032013 Industrial Functions
+//Is the waiting room big enough for all the children scheduled for appointments today?
+
+prompt("I am figuring out if the waiting room is big enough for all the children scheduled today! What is the width of the waiting room in feet?");
+prompt("What is the height of the waiting room?");
+prompt("How many children are scheduled today?");
+
