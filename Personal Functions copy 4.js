c3b58de931d60956e5ed2a3bfa09960b6c9cf88c
nelson12# <aw.nelson@hotmail.com>
2013-08-04 21:29:09 -0400

    Printing to console

---
+++ Personal Functions copy 4.js
@@ -0,0 +1,13 @@
+//adam nelson 08032013 Personal Functions
+//What will happen if my daughter wakes up too many times in one night?
+
+function timesAwake(){
+	var a = prompt("I am figuring out what happens when a child wakes up too mnay 	    times a night! How many times does Izzy wake up between 8 and midnight?");    //declaring variable
+	var b = prompt("How many times does she wake up between midnight and 8am?");
+	//declaring variable
+	var c = a*b;//stating equation
+	console.log(c);//printing to the console
+}
+
+	
+	
